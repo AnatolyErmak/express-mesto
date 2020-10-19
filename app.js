df86c14a62fbf91d9fb9e61acd18f20ce963bef0
Anatoly Ermakov <Pr0f3ssAr@yandex.ru>
2020-10-19 22:36:44 +0300

    12 complete

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 const express = require('express');
+const path = require('path');
+
+const usersRoutes = require('./routes/users');
+const cardsRoutes = require('./routes/cards');
 
 const { PORT = 3000 } = process.env;
-
-const path = require('path');
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/', usersRoutes);
+app.use('/', cardsRoutes);
+
+app.all('*', (req, res) => {
+  res.status(404).send({ message: 'Запрашиваемый ресурс не найден' });
+});
+
 app.listen(PORT, () => {
   // Если всё работает, консоль покажет, какой порт приложение слушает
-  console.log(`App listening on port ${PORT}`);
+  console.log(`Ссылка на сервер ${PORT}`);
 });
